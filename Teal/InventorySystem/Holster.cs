cba1e4c02be183958fcf48e86aa4aa5be85ada71
Teal <97361953+TealDealMeal@users.noreply.github.com>
2025-09-25 19:20:37 +0200

    v3

---
+++ Teal/InventorySystem/Holster.cs
@@ -0,0 +1,220 @@
+﻿using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+using System;
+using VRC.Udon.Common;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class Holster : UdonSharpBehaviour {
+    [NonSerialized] bool holdingItem;
+    [NonSerialized] VRCPlayerApi localPlayer;
+
+    [NonSerialized] ushort itemId;
+    [NonSerialized] GameObject itemObject;
+    [NonSerialized] Transform itemTransform;
+    [NonSerialized] VRC_Pickup itemPickup;
+
+    [NonSerialized] bool handCollision;
+    [NonSerialized] bool rightHandCollision;
+    [NonSerialized] bool leftHandCollision;
+
+    // Automated
+    [HideInInspector] public Inventory inventory;
+    [HideInInspector] public MeshRenderer meshRenderer;
+
+    void Start() {
+        localPlayer = Networking.LocalPlayer;
+    }
+
+    public override void PostLateUpdate() {
+        if (!itemPickup) return;
+
+        if (holdingItem) {
+            if (itemPickup.IsHeld) {
+                _ReleaseItem();
+                return;
+            }
+
+            // We now have to update transforms, it appears VRChat recently broke parenting for VRC Object Sync? (seen in v2)
+            itemTransform.localPosition = Vector3.zero;
+            itemTransform.localRotation = Quaternion.identity;
+        } else if (!itemPickup.IsHeld) {
+            if (Networking.GetOwner(itemPickup.gameObject) != localPlayer) { // Item was stolen
+                _UnassignItem();
+
+                return;
+            }
+
+            _HoldItem();
+        }
+    }
+
+    // Insert items
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == inventory.handLayer) {
+            bool usingLeftHand = false;
+
+            if (other.gameObject == inventory.rightHand)
+                rightHandCollision = true;
+            else {
+                leftHandCollision = true;
+                usingLeftHand = true;
+            }
+
+            handCollision = true;
+
+            if (holdingItem && itemPickup && inventory.handCollidersAccessHolsters)
+                itemPickup.pickupable = true;
+            else if (!holdingItem && !itemPickup && inventory.handCollidersPlaceItems) {
+                VRC_Pickup _pickup = localPlayer.GetPickupInHand(!usingLeftHand ? VRC_Pickup.PickupHand.Right : VRC_Pickup.PickupHand.Left);
+
+                if (_pickup && ((1 << _pickup.gameObject.layer) & inventory.holsterableItemLayers) != 0) {
+                    for (byte i = 0; i < inventory.holsterCount; i++)
+                        if (inventory.holsters[i].itemObject == _pickup.gameObject)
+                            inventory.holsters[i]._UnassignItem(); // Maybe we want to put it in the other slot?
+
+                    if (!usingLeftHand)
+                        localPlayer.PlayHapticEventInHand(VRC_Pickup.PickupHand.Right, 0.2f, 0.25f, 90);
+                    else
+                        localPlayer.PlayHapticEventInHand(VRC_Pickup.PickupHand.Left, 0.2f, 0.25f, 90);
+
+                    _AssignItem(_pickup.gameObject);
+                }
+            }
+
+            return;
+        }
+
+        if (holdingItem || inventory.handCollidersPlaceItems || itemPickup) return;
+
+        GameObject _itemObject = other.gameObject;
+
+        if (((1 << other.gameObject.layer) & inventory.holsterableItemLayers) == 0) return;
+
+        VRC_Pickup rightHand = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Right);
+        VRC_Pickup leftHand = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Left);
+
+        bool isRightHand = rightHand && rightHand.gameObject == _itemObject ? true : false;
+        bool isLeftHand = leftHand && leftHand.gameObject == _itemObject ? true : false;
+
+        if (isRightHand || isLeftHand) {
+            for (byte i = 0; i < inventory.holsterCount; i++)
+                if (inventory.holsters[i].itemObject == _itemObject)
+                    inventory.holsters[i]._UnassignItem(); // Maybe we want to put it in the other slot?
+
+            if (isRightHand)
+                localPlayer.PlayHapticEventInHand(VRC_Pickup.PickupHand.Right, 0.2f, 0.1f, 67);
+            else
+                localPlayer.PlayHapticEventInHand(VRC_Pickup.PickupHand.Left, 0.2f, 0.1f, 67);
+
+            _AssignItem(_itemObject);
+        }
+    }
+
+    public void _AssignItem(GameObject _itemObject) {
+        if (ushort.TryParse(_itemObject.name, out ushort pickupId)) {
+            itemId = pickupId;
+            itemObject = _itemObject;
+            itemTransform = _itemObject.transform;
+            itemPickup = inventory.pickupArray[itemId];
+
+            meshRenderer.material = inventory.filledHolsterMaterial;
+        }
+    }
+
+    public void _HoldItem(bool dontRegister = false) {
+        if (!itemObject) return;
+
+        holdingItem = true;
+        if (!dontRegister)
+            inventory._SetItemState(itemId, true);
+
+        Transform _transform = itemObject.transform;
+        _transform.parent = transform;
+        _transform.localPosition = Vector3.zero;
+        _transform.localRotation = Quaternion.identity;
+
+        SendCustomEventDelayedSeconds(nameof(_EnableItemPickup), inventory.holsterDelay);
+    }
+
+    public void _EnableItemPickup() {
+        if (!itemPickup || inventory.handCollidersAccessHolsters && !handCollision) return;
+
+        itemPickup.pickupable = true;
+    }
+
+    public void _ForceHolster(GameObject _itemObject, bool dontRegister = false) {
+        if (!Networking.IsOwner(_itemObject))
+            Networking.SetOwner(localPlayer, _itemObject);
+
+        _AssignItem(_itemObject);
+        _HoldItem(dontRegister);
+    }
+
+    // Remove items
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.layer == inventory.handLayer) {
+            bool usingLeftHand = false;
+
+            if (other.gameObject == inventory.rightHand)
+                rightHandCollision = false;
+            else {
+                leftHandCollision = false;
+                usingLeftHand = true;
+            }
+
+            handCollision = rightHandCollision == true || leftHandCollision == true;
+
+            if (holdingItem && itemPickup && inventory.handCollidersAccessHolsters && !handCollision)
+                itemPickup.pickupable = false;
+            else if (!holdingItem && inventory.handCollidersPlaceItems && !inventory.keepItemsInHolster) {
+                VRC_Pickup _pickup = localPlayer.GetPickupInHand(!usingLeftHand ? VRC_Pickup.PickupHand.Right : VRC_Pickup.PickupHand.Left);
+
+                if (_pickup == itemPickup)
+                    _UnassignItem();
+            }
+
+            return;
+        }
+
+        if (inventory.keepItemsInHolster || inventory.handCollidersPlaceItems && handCollision || !itemObject || other.gameObject != itemObject) return;
+
+        VRC_Pickup rightHand = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Right);
+        VRC_Pickup leftHand = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Left);
+
+        // Make sure we hold the item too, otherwise it drops out of our holster
+        if (rightHand != itemPickup && leftHand != itemPickup) return;
+
+        _UnassignItem();
+    }
+
+    public void _UnassignItem() {
+        itemId = ushort.MaxValue;
+        itemObject = null;
+        itemTransform = null;
+        itemPickup = null;
+
+        meshRenderer.material = inventory.emptyHolsterMaterial;
+    }
+
+    public void _ReleaseItem(bool dontRegister = false) {
+        if (!itemObject) return;
+
+        itemPickup.pickupable = true;
+        if (!dontRegister)
+            inventory._SetItemState(itemId, false);
+
+        itemObject.transform.SetParent(null);
+
+        holdingItem = false;
+    }
+
+    public void _ForceDrop(bool dontRegister = false) {
+        _ReleaseItem(dontRegister);
+        _UnassignItem();
+    }
+
+    public override bool OnOwnershipRequest(VRCPlayerApi requester, VRCPlayerApi newOwner) {
+        return false;
+    }
+}
---
+++ Teal/InventorySystem/Inventory.cs
@@ -0,0 +1,331 @@
+﻿using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+using VRC.SDK3.Components;
+using VRC.SDK3.Data;
+using System;
+using static VRC.SDKBase.VRCPlayerApi;
+using VRC.Udon.Common.Interfaces;
+using VRC.SDK3.UdonNetworkCalling;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class Inventory : UdonSharpBehaviour {
+    [Tooltip("Determines by layer which pickup can be holstered.")]
+    public LayerMask holsterableItemLayers = 1 << 13;
+    public Transform disconnectSyncTransform;
+
+    [Header("Inventory Behaviour")]
+    [Tooltip("If turned off, items will stay on the player when respawned.")]
+    public bool dropItemsOnRespawn = true;
+
+    [Tooltip("Items will respawn when the player respawns, if turned off, they will be dropped instead.")]
+    public bool respawnItemsOnRespawn = false;
+
+    [Tooltip("Items will respawn when the player disconnects, if turned off, they will be dropped instead.")]
+    public bool respawnItemsOnDisconnect = false;
+
+    [Header("Inventory Rotation")]
+    [Tooltip("Lock the inventory rotation in place, automatically disabled for non-VR players.")]
+    public bool lockInventoryRotation = true;
+
+    [Tooltip("How fast the inventory rotates.")]
+    public float rotationSpeed = 1.5f;
+
+    [Tooltip("How far the inventory rotates.")]
+    public byte maxRotation = 72;
+
+    [Header("Holster Behaviour")]
+    [Tooltip("Turns off items for other players, probably also reduces bandwidth, as items will no longer be synced while turned off.")]
+    public bool disableItemsWhenHolstered = true;
+
+    [Tooltip("When attempting to drop an item, the item will return to the holster. The target holster may be modified by placing the item into another empty holster.")]
+    public bool keepItemsInHolster = false;
+
+    [Header("Hand Colliders")]
+    [Tooltip("Items within the holsters will only become interactable, if the hand collider touches the holster. This will help with VRChat's notoriously bad pick up detection, automatically disabled for non-VR players.")]
+    public bool handCollidersAccessHolsters = true;
+    
+    [Tooltip("You can only place items in the holster if your hand collider touches them, automatically disabled for non-VR players.")]
+    public bool handCollidersPlaceItems = true;
+
+    [Tooltip("Displays the hand colliders, doesn't work for non-VR players.")]
+    public bool debugHandColliders = false;
+
+    [Header("Misc")]
+    [Tooltip("Delay before being able to interact with the holster again, should prevent syncing inconsistencies.")]
+    public float holsterDelay = 0.1f;
+    public Material emptyHolsterMaterial;
+    public Material filledHolsterMaterial;
+
+    [NonSerialized] VRCPlayerApi localPlayer;
+    [NonSerialized] Transform holsterParent;
+    [NonSerialized] bool lateJoiner = true;
+
+    [NonSerialized] public int handLayer;
+    [NonSerialized] public bool trackHands;
+    [NonSerialized] public GameObject rightHand;
+    [NonSerialized] TrackingDataType headTracking = TrackingDataType.Head;
+    [NonSerialized] TrackingDataType rightHandTracking = TrackingDataType.RightHand;
+    [NonSerialized] TrackingDataType leftHandTracking = TrackingDataType.LeftHand;
+
+    [NonSerialized] bool usedInventory;
+    [NonSerialized] bool[] localPickupArrayHolstered;
+    [NonSerialized] ushort localPickupHolsteredCount;
+
+    [HideInInspector, UdonSynced] public ushort pickupHolsteredCount;
+    [HideInInspector, UdonSynced] public int[] pickupArrayPlayerId;
+
+    // Automated
+    [HideInInspector] public Holster[] holsters;
+    [HideInInspector] public byte holsterCount;
+
+    [HideInInspector] public VRC_Pickup[] pickupArray;
+    [HideInInspector] public ushort pickupCount;
+
+    [HideInInspector] public bool pickupsCanRespawn;
+    [HideInInspector] public VRCObjectSync[] pickupArrayVRCObjectSync;
+    [HideInInspector] public Vector3[] pickupArrayRespawnPos; // Fallback, in case VRCObjectSync doesn't exist
+    [HideInInspector] public Rigidbody[] pickupArrayRigidbody;
+    [HideInInspector] public Collider[] pickupArrayCollider;
+    [HideInInspector] public bool[] pickupArrayUseGravity;
+    [HideInInspector] public bool[] pickupArrayIsKinematic;
+
+    void Start() {
+        localPlayer = Networking.LocalPlayer;
+        holsterParent = transform.GetChild(0);
+
+        headTracking = TrackingDataType.Head;
+        handLayer = transform.GetChild(1).gameObject.layer;
+
+        localPickupArrayHolstered = new bool[pickupCount];
+
+        for (ushort i = 0; i < pickupCount; i++)
+            pickupArray[i].name = i.ToString();
+
+        if (!localPlayer.IsUserInVR()) {
+            lockInventoryRotation = false;
+            handCollidersAccessHolsters = false;
+            handCollidersPlaceItems = false;
+        }
+
+        if (handCollidersAccessHolsters || handCollidersPlaceItems) {
+            trackHands = true;
+            rightHand = transform.GetChild(1).gameObject;
+
+            rightHand.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(true);
+
+            rightHandTracking = TrackingDataType.RightHand;
+            leftHandTracking = TrackingDataType.LeftHand;
+
+            if (debugHandColliders) {
+                transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(2).GetChild(0).gameObject.SetActive(true);
+            }
+        }
+
+        if (localPlayer.isMaster) {
+            lateJoiner = false;
+            RequestSerialization();
+        }
+    }
+
+    public override void PostLateUpdate() {
+        if (trackHands) {
+            TrackingData rightHandData = localPlayer.GetTrackingData(rightHandTracking);
+            transform.GetChild(1).SetPositionAndRotation(rightHandData.position, rightHandData.rotation);
+
+            TrackingData leftHandData = localPlayer.GetTrackingData(leftHandTracking);
+            transform.GetChild(2).SetPositionAndRotation(leftHandData.position, leftHandData.rotation);
+        }
+
+        TrackingData headData = localPlayer.GetTrackingData(headTracking);
+
+        holsterParent.position = headData.position;
+
+        float inventoryRotation = headData.rotation.eulerAngles.y;
+        float angle = inventoryRotation;
+
+        if (!lockInventoryRotation) {
+            angle = Mathf.LerpAngle(holsterParent.eulerAngles.y, inventoryRotation, rotationSpeed * Time.deltaTime);
+            if (angle - maxRotation < inventoryRotation && angle + maxRotation > inventoryRotation) return;
+        }
+
+        holsterParent.eulerAngles = new Vector3(0, angle, 0);
+    }
+
+    public void _SetItemState(ushort _pickupId, bool _pickupWasHolstered) {
+        if (!_pickupWasHolstered && pickupArrayPlayerId[_pickupId] == 0 || _pickupWasHolstered && pickupArrayPlayerId[_pickupId] != 0) return;
+
+        usedInventory = true;
+        SendCustomNetworkEvent(NetworkEventTarget.All, nameof(_SyncItemState), localPlayer.playerId, _pickupId, _pickupWasHolstered);
+    }
+
+    [NetworkCallable]
+    public void _SyncItemState(int _playerId, ushort _pickupId, bool _pickupWasHolstered) {
+        pickupArrayPlayerId[_pickupId] = _pickupWasHolstered ? _playerId : 0;
+        pickupHolsteredCount = (ushort)Mathf.Max(_pickupWasHolstered ? pickupHolsteredCount + 1 : pickupHolsteredCount - 1, 0);
+
+        _SetItemHolstered(_pickupId, _pickupWasHolstered, false);
+
+        RequestSerialization();
+    }
+
+    public void _SetItemHolstered(ushort _pickupId, bool _pickupWasHolstered, bool _respawnItems) {
+        if (_pickupId < 0 || _pickupId >= pickupCount) return;
+
+        localPickupArrayHolstered[_pickupId] = _pickupWasHolstered;
+        localPickupHolsteredCount = (ushort)Mathf.Max(_pickupWasHolstered ? localPickupHolsteredCount + 1 : localPickupHolsteredCount - 1, 0);
+
+        VRC_Pickup _pickup = pickupArray[_pickupId];
+        Rigidbody _rigidbody = pickupArrayRigidbody[_pickupId];
+
+        if (_pickupWasHolstered) {
+            _pickup.pickupable = false;
+
+            _rigidbody.useGravity = false;
+            _rigidbody.isKinematic = true;
+
+            if (disableItemsWhenHolstered && localPlayer != Networking.GetOwner(_pickup.gameObject))
+                _pickup.gameObject.SetActive(false);
+        } else {
+            _pickup.pickupable = true;
+
+            _rigidbody.useGravity = pickupArrayUseGravity[_pickupId];
+            _rigidbody.isKinematic = pickupArrayIsKinematic[_pickupId];
+
+            if (disableItemsWhenHolstered)
+                _pickup.gameObject.SetActive(true);
+
+            if (pickupsCanRespawn && _respawnItems) {
+                VRCObjectSync objectSync = pickupArrayVRCObjectSync[_pickupId];
+
+                if (objectSync)
+                    objectSync.Respawn();
+                else
+                    _pickup.transform.position = pickupArrayRespawnPos[_pickupId];
+            }
+        }
+    }
+
+    public override void OnPlayerRespawn(VRCPlayerApi player) {
+        // Only called locally
+
+        if (dropItemsOnRespawn || respawnItemsOnRespawn)
+            _DropInventory();
+    }
+
+    public void _DropInventory() {
+        if (!usedInventory) return;
+        usedInventory = false;
+
+        int playerId = localPlayer.playerId;
+
+        for (byte i = 0; i < holsterCount; i++)
+            holsters[i]._ForceDrop(true);
+
+        ushort[] _pickups = new ushort[holsterCount];
+        byte _currHolster = 0;
+
+        for (ushort i = 0; i < pickupCount; i++) {
+            if (pickupArrayPlayerId[i] != playerId) continue;
+
+            _pickups[_currHolster] = (ushort)(i + 1);
+            _currHolster++;
+
+            if (_currHolster == holsterCount) break;
+        }
+
+        SendCustomNetworkEvent(NetworkEventTarget.All, nameof(_SyncDropInventory), _pickups); //new DataToken(_pickups)
+    }
+
+    [NetworkCallable]
+    public void _SyncDropInventory(ushort[] _pickups) {
+        foreach (ushort _pickupId in _pickups) {
+            if (_pickupId == 0) continue;
+
+            pickupArrayPlayerId[_pickupId - 1] = 0;
+            pickupHolsteredCount = (ushort)Mathf.Max(pickupHolsteredCount - 1, 0);
+
+            _SetItemHolstered((ushort)(_pickupId - 1), false, respawnItemsOnRespawn);
+        }
+
+        RequestSerialization();
+    }
+
+    public override void OnPlayerLeft(VRCPlayerApi player) {
+        int playerId = player.playerId;
+        Vector3 playerPos = player.GetPosition() + Vector3.up * 0.25f;
+
+        for (ushort i = 0; i < pickupCount; i++) {
+            if (pickupArrayPlayerId[i] != playerId) continue;
+
+            pickupArrayPlayerId[i] = 0;
+            pickupHolsteredCount = (ushort)Mathf.Max(pickupHolsteredCount - 1, 0);
+            _DisconnectItemUnholster(i, respawnItemsOnDisconnect, playerPos);
+        }
+
+        RequestSerialization();
+    }
+
+    public void _DisconnectItemUnholster(ushort _pickupId, bool _respawnItems, Vector3 _playerPos) {
+        if (_pickupId < 0 || _pickupId >= pickupCount) return;
+
+        localPickupArrayHolstered[_pickupId] = false;
+        localPickupHolsteredCount = (ushort)Mathf.Max(localPickupHolsteredCount - 1, 0);
+
+        VRC_Pickup _pickup = pickupArray[_pickupId];
+        Rigidbody _rigidbody = pickupArrayRigidbody[_pickupId];
+
+        _pickup.pickupable = true;
+
+        _rigidbody.useGravity = pickupArrayUseGravity[_pickupId];
+        _rigidbody.isKinematic = pickupArrayIsKinematic[_pickupId];
+
+        if (disableItemsWhenHolstered)
+            _pickup.gameObject.SetActive(true);
+
+        if (pickupsCanRespawn && _respawnItems) {
+            VRCObjectSync objectSync = pickupArrayVRCObjectSync[_pickupId];
+
+            if (objectSync)
+                objectSync.Respawn();
+            else
+                _pickup.transform.position = pickupArrayRespawnPos[_pickupId];
+        } else {
+            VRCObjectSync objectSync = pickupArrayVRCObjectSync[_pickupId];
+
+            if (objectSync) {
+                disconnectSyncTransform.position = _playerPos;
+                objectSync.TeleportTo(disconnectSyncTransform);
+            } else
+                _pickup.transform.position = _playerPos;
+        }
+    }
+
+    public override void OnDeserialization() {
+        if (lateJoiner) {
+            for (ushort i = 0; i < pickupCount; i++) {
+                if (pickupArrayPlayerId[i] == 0) continue;
+
+                _SetItemHolstered(i, true, false);
+            }
+
+            lateJoiner = false;
+            return;
+        }
+
+        if (pickupHolsteredCount != localPickupHolsteredCount) { // Desync fix
+            for (ushort i = 0; i < pickupCount; i++) {
+                bool wasHolstered = pickupArrayPlayerId[i] != 0;
+                if (localPickupArrayHolstered[i] == wasHolstered) continue;
+
+                _SetItemHolstered(i, wasHolstered, false);
+            }
+        }
+    }
+
+    public override bool OnOwnershipRequest(VRCPlayerApi requester, VRCPlayerApi newOwner) {
+        return false;
+    }
+}
